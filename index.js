58f268cea02092619c0ede2d07d942e7d3bf5946
Alessandro Mamolo <71407691+TurboWrestler250@users.noreply.github.com>
2026-05-13 16:43:51 +0200

    update index.js

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ require('dotenv').config()
 // or import 'dotenv/config' // for esm
 
 console.log(`Hello ${process.env.HELLO}`)
+
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
